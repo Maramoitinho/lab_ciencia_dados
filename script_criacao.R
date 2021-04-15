ea740e4e4c250aedc81e45ef83fcf760b6ea56ad
Maramoitinho <maramoitinho@gmail.com>
2021-04-14 21:09:46 -0300

    Primeiro commit

--- script_criacao.R
+++ script_criacao.R
@@ -1,1 +1,7 @@
 #usethis::create_project("c:\\projetos_R\\lab_ciencia_dados")
+
+#criar a estrutura Git para esse projeto
+usethis::use_git()
+usethis::use_github()
+usethis::github_token()
+usethis::create_github_token()
